d19fc8b47bd91900c723a2b1a2894b955755a0b5
David Lin <d2955388365@gmail.com>
2022-06-13 00:13:42 +0800

    add mvc version

---
+++ api/api_attractions.py
@@ -0,0 +1,81 @@
+from flask import *
+import json
+import ast
+import decimal
+from cnxpool import cnxpool
+
+api_attractions=Blueprint("api_attractions", __name__, template_folder="templates")
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        super(DecimalEncoder, self).default(o)
+
+
+
+@api_attractions.route("/api/attractions")
+def attractions():
+	try:
+		cnx=cnxpool.get_connection()
+		mycursor=cnx.cursor() 
+		page = request.args.get("page")
+		keyword = request.args.get("keyword")
+		if page != '' and keyword == None:	
+			page = int(page)
+			page_index = page*12
+			sql = "SELECT id, name, category, description, address, transport, mrt, latitude, longitude, images FROM attractions ORDER BY id LIMIT %s, %s"
+			adr = (page_index,12)		
+			mycursor.execute(sql, adr)
+			myresult = mycursor.fetchall()
+			names = 'id name category description address transport mrt latitude longitude images'.split()
+			data = [{name:value for name, value in zip(names, mr)} for mr in myresult]
+			data = json.dumps(data, cls=DecimalEncoder, ensure_ascii=False)
+			data = ast.literal_eval(data)
+			c = len(data)
+			if c <12:
+				nextPage = "null"
+			else:
+				nextPage = page+1	
+			for i in data:
+				i["images"] = ast.literal_eval(i["images"])
+			y = {
+				"nextPage":nextPage,
+				"data":data
+			}
+			return jsonify(y)
+		elif page != '' and keyword != None:
+			page = int(page)
+			page_index = page*12
+			sql = "SELECT id, name, category, description, address, transport, mrt, latitude, longitude, images FROM attractions WHERE name LIKE %s ORDER BY id LIMIT %s, %s "
+			adr = ('%'+keyword+'%',page_index,12)
+			nextPage = page+1
+			mycursor.execute(sql,adr)
+			myresult = mycursor.fetchall()
+			names = 'id name category description address transport mrt latitude longitude images'.split()
+			data = [{name:value for name, value in zip(names, mr)} for mr in myresult]
+			data = json.dumps(data, cls=DecimalEncoder, ensure_ascii=False)
+			data = ast.literal_eval(data)
+			c = len(data)
+			if c <12:
+				nextPage = "null"
+			else:
+				nextPage = page+1
+			for i in data:
+				i["images"] = ast.literal_eval(i["images"])
+			y = {
+				"nextPage":nextPage,
+				"data":data
+			}
+			return jsonify(y)
+		else:
+			a = 1
+			x = {
+				"error": bool(a),
+				"message":"page沒輸入"
+			}
+			return jsonify(x)
+	finally:
+		if cnx.in_transaction:
+			cnx.rollback()
+		cnx.close()
---
+++ api/api_booking.py
@@ -0,0 +1,145 @@
+from flask import *
+import ast
+from cnxpool import cnxpool
+
+
+api_booking=Blueprint("api_booking", __name__, template_folder="templates")
+
+
+@api_booking.route("/api/booking")
+def bookinfo():
+	try:
+		if "e_mail" in session :
+			cnx=cnxpool.get_connection()
+			mycursor=cnx.cursor()
+			email = session["e_mail"]
+			sql2 = "SELECT attractionid,name,address,image,date,time,price,email FROM attractions2 WHERE email = %s"
+			adr2 = (email,)
+			mycursor.execute(sql2, adr2)
+			myresult = mycursor.fetchall()
+			if myresult != []:
+				x = myresult[0]
+				x.__str__()
+				attractionId = x[0]
+				name = x[1]
+				address = x[2]
+				image = x[3]
+				date = x[4]
+				time = x[5]
+				price = x[6]
+				email = x[7]
+				return jsonify({
+					"data": {
+						"attraction": {
+						"attractionid": attractionId,
+						"name":name,
+						"address":address,
+						"image" :image
+						},
+						"date":date,
+						"time":time,
+						"price":price		
+					}
+				})
+			else:
+				return jsonify({
+					"data":None
+				})
+		else:
+			return jsonify({
+				"error": True,
+				"message": "未登入系統，拒絕存取"
+			}),403
+	finally:
+		if cnx.in_transaction:
+			cnx.rollback()
+		cnx.close()	
+
+
+@api_booking.route("/api/booking",methods=["POST"])
+def createinfo():
+	try :
+		if "e_mail" in session :
+			cnx=cnxpool.get_connection()
+			mycursor=cnx.cursor()
+			req = request.get_json()
+			price = req["price"]
+			priceslice = int(price[-5:-1])
+			morning = req["morningVlue"]
+			y =""	
+			if morning == True:
+				y ="上半天"
+			else:
+				y ="下半天"			
+			attractionId = req["id"]
+			sql = "SELECT name,address,images FROM attractions WHERE id = %s" 
+			adr = (attractionId,)
+			mycursor.execute(sql, adr)
+			myresult = mycursor.fetchall()
+			x = myresult[0]
+			x.__str__()
+			name = x[0]
+			address = x[1]
+			image =ast.literal_eval(x[2])[0]
+			date = req["date"]
+			email = session["e_mail"]
+			price = priceslice	
+			time = y		
+			if date !="":
+				sql = ("INSERT INTO attractions2(attractionid,name,address,image,date,time,price,email)" 
+				" VALUES(%s,%s,%s,%s,%s,%s,%s,%s) ON duplicate KEY UPDATE" 
+				"`attractionid`=VALUES(`attractionid`),`name`=VALUES(`name`),`address`=VALUES(`address`),`image`=VALUES(`image`),`date`=VALUES(`date`),`time`=VALUES(`time`),`price`=VALUES(`price`),`email`=VALUES(`email`)")		
+				adr = (attractionId,name,address,image,date,time,price,email)
+				
+				
+				cnx.commit()			
+				return jsonify({
+					"attractionId":attractionId,
+					"date": date,
+					"time": y,
+					"price": priceslice
+				})
+			else:
+				return jsonify({
+					"error": True,
+					"message": "建立失敗，日期沒輸入"
+				}),400
+		else:					
+			return jsonify({
+				"error": True,
+				"message": "未登入系統，拒絕存取"
+			}),403	
+	except:
+		return jsonify({
+			"error": True,
+			"message": "伺服器內部錯誤"
+		}),500		
+	finally:
+		if cnx.in_transaction:
+			cnx.rollback()
+		cnx.close()
+
+
+@api_booking.route("/api/booking", methods=["DELETE"])
+def deleteBook():
+	try:
+		if session != {}:
+			cnx=cnxpool.get_connection()
+			mycursor=cnx.cursor()			
+			email = session["e_mail"]
+			sql3 = "DELETE FROM attractions2 where email = %s"
+			adr3 = (email,)
+			mycursor.execute(sql3, adr3)
+			cnx.commit()
+			return jsonify({
+				"ok": True
+			})
+		else:
+			return jsonify({
+				"error": True,
+				"message": "未登入系統，拒絕存取"
+			}),403
+	finally:
+		if cnx.in_transaction:
+			cnx.rollback()
+		cnx.close()
